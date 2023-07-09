bcbcdc7736b50e3e8919383820a7057168cc7c49
limseongkyu <96x60812@gmail.com>
2023-07-09 16:37:04 +0900

    Feat: Function

---
+++ src/main/kotlin/function/Main.kt
@@ -0,0 +1,26 @@
+package function
+
+
+/*
+function
+
+named arguments
+- if you call function with named arguments, you can call function with any order arguments.
+
+default arguments
+- you can declare default value of arguments and it can be omitted.
+
+function without return (void in java)
+
+single-expression functions
+
+- if there are no body in function, culry braces can be omitted and = can be used.
+
+
+lambda expressions
+
+for example .filter, .map
+
+
+ */
+fun main(){ }
