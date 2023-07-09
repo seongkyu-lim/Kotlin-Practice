2ca5c40d693906ade5a16d6a98e06cf74c8a47ce
limseongkyu <96x60812@gmail.com>
2023-07-09 16:58:57 +0900

    Feat: Class

--- src/main/kotlin/class/Basic.kt
+++ src/main/kotlin/class/Basic.kt
@@ -11,7 +11,16 @@ fun main() {
     rectangle.area = 4
     println(rectangle.area)
 }
+/*
+kotlin에서는 pranthesis 안에도 멤버변수들 선언할 수 있다.
 
+new Keyword 없이 instance 생성 가능.
+
+constructor, data classes.
+
+
+
+ */
 class Basic constructor(
     private val name: String,
     val age: Int
--- src/main/kotlin/function/Main.kt
+++ src/main/kotlin/function/Main.kt
@@ -23,4 +23,10 @@ for example .filter, .map
 
 
  */
-fun main(){ }
+fun main(){
+    val actions = listOf("title", "year", "author")
+    val prefix = "https://example.com/book-info"
+    val id = 5
+    val urls = actions.map { action -> "$prefix/$id/$action" }
+    println(urls)
+}
